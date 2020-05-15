1d5b847713b5468c7248642b8dca98f821334c4e
Matheus Batista Teixeira <mathbteixeira@gmail.com>
2020-05-15 10:40:46 -0300

    Microservices de Fornecedor, Loja e Eureka-Server

---
+++ loja/src/test/java/br/com/alura/microservice/loja/LojaApplicationTests.java
@@ -0,0 +1,12 @@
+package br.com.alura.microservice.loja;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaApplicationTests {
+
+	@org.junit.Test
+	void contextLoads() {
+	}
+
+}
